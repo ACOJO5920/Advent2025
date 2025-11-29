11e90e82b986e095544d4cc8e6c61e70d719385d
ACOJO5920 <andrei85@hotmail.co.uk>
2025-11-29 17:06:31 +0000

    add extra things

--- practice_2024.py
+++ practice_2024.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass
+import string
 
 
 @dataclass
 class FileData:
     left_list: list[int]
     right_list: list[int]
+
+    @property
+    def similarity_score(self):
+        return 1
 
 
 DEFAULT_FILENAME = "input_practice.txt"
@@ -17,7 +22,40 @@ def read_lists(file_name: str = DEFAULT_FILENAME) -> FileData:
     :param file_name: the file name to read from
     :return: a FileData object containing the two lists in the file
     """
-    pass
+    left_list: list[int] = []
+    right_list: list[int] = []
+
+    with open(file_name) as f:
+        for line in f.readlines():
+            line_values = extract_values_from_line(line)
+            left_list.append(line_values[0])
+            right_list.append(line_values[1])
+
+    return FileData(left_list, right_list)
+
+
+def extract_values_from_line(line: str) -> tuple[int, int]:
+    """
+    Extracts two values with whitespace in between
+
+    :param line: the line to extract values from
+    :return: a tuple containing both values
+    """
+    line = line.rstrip()
+
+    left_value = ""
+    ix = 0
+    while ix < len(line) and line[ix] not in string.whitespace:
+        left_value += line[ix]
+        ix += 1
+
+    right_value = ""
+    ix = len(line) - 1
+    while ix > -1 and line[ix] not in string.whitespace:
+        right_value = line[ix] + right_value
+        ix -= 1
+
+    return int(left_value), int(right_value)
 
 
 def do_part_1(input_file_name: str = DEFAULT_FILENAME) -> int:
@@ -27,4 +65,6 @@ def do_part_1(input_file_name: str = DEFAULT_FILENAME) -> int:
     :param input_file_name: the input file to complete
     :return: the similarity score of the input file
     """
-    pass
+    file_data = read_lists(input_file_name)
+
+    return file_data.similarity_score
--- test_practice_2024.py
+++ test_practice_2024.py
@@ -1,4 +1,5 @@
-from practice_2024 import do_part_1
+import pytest
+from practice_2024 import do_part_1, read_lists, extract_values_from_line, FileData
 
 
 class Test2024Day1:
@@ -7,3 +8,31 @@ class Test2024Day1:
         Tests that running the part 1 function gets me the right answer
         """
         assert do_part_1("input_practice.txt") == 26800609
+
+    def test_read_file(self):
+        """
+        Tests that reading the data from a test file gives the correct FileData object
+        """
+        file_data = read_lists("simple_input_practice.txt")
+        assert file_data.left_list == [3, 4, 2, 1, 3, 3]
+        assert file_data.right_list == [4, 3, 5, 3, 9, 3]
+
+    def test_extract_values_from_line(self):
+        """
+        Tests that the correct values are extracted from a given line
+        """
+        assert extract_values_from_line("80784   47731") == (80784, 47731)
+
+
+class TestFileData:
+    """
+    Tests the FileData class
+    """
+
+    @pytest.mark.parametrize("list1,list2,expected_score", [([1, 2, 3], [3, 2, 1], 0)])
+    def test_similarity_score(self, list1, list2, expected_score):
+        """
+        Tests that the similarity score function generates the correct value
+        """
+        file_data = FileData(list1, list2)
+        assert file_data.similarity_score == expected_score
